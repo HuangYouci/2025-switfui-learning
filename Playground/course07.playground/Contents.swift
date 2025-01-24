c7ec90ba3a11f3410c2dadc01c0fa3e57f45aea5
Hunag Youci <huangyouci@icloud.com>
2025-01-24 14:39:42 +0800

    20250124-push02
    Update .gitignore to apply to all "*.DS_Store"
    
    - Create course07.playground for course07 (Functions)
    - Create course08.playground for course08 (Turple)

---
+++ Playground/course07.playground/Contents.swift
@@ -0,0 +1,6 @@
+func multiply(_ a: Int) -> Int
+{
+    return a*2
+}
+
+print(multiply(4))
---
+++ Playground/course08.playground/Contents.swift
@@ -0,0 +1,7 @@
+typealias Human = (name: String, height: Double, hairColor: String)
+
+let Benson: Human = ("班森", 180, "黃")
+
+print("\(Benson.0) 有 \(Benson.1) 公分，留著 \(Benson.2) 色頭髮")
+
+print("\(Benson.name) 有 \(Benson.height) 公分，留著 \(Benson.hairColor) 色頭髮")
