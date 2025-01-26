648b26622becc67ef6fb962b20b974692a7f7ad2
Hunag Youci <huangyouci@icloud.com>
2025-01-26 16:04:15 +0800

    20250126-push01
    2025 年 01 月 26 日 推送 01
    Push 01 on January 26, 2025
    
    - course01 專案完成一個小節，目前可以實現基礎功能，且有初步介面設計。（影片 7）The course01 project has completed one section, with basic functionalities implemented and an initial interface design in place. (Video 7)

--- course01/course01/ContentView.swift
+++ course01/course01/ContentView.swift
@@ -9,34 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let food = ["漢堡", "沙拉", "披薩", "義大利麵", "雞腿便當", "刀削麵", "火鍋", "牛肉麵", "關東煮"]
+    let food = Food.examples
     
-    @State private var selectedFood: String?
+    @State private var selectedFood: Food?
+    @State private var showInfo: Bool = false
     
     var body: some View {
         VStack (spacing: 30) {
-            Image("dinner")
-                .resizable()
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+            
+            if selectedFood != .none {
+                
+                Text(selectedFood!.image)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.7)
+                    .lineLimit(1)
+                    
+            } else {
+                
+                Image("dinner")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+            }
                 
             Text("今天吃什麼？")
                 .font(.title)
                 .bold()
             
-            if (selectedFood != ""){
+            if selectedFood != .none {
                 
-                Text(selectedFood ?? "")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundStyle(.green)
-                    .transition(.scale)
+                HStack{
+                    
+                    Text(selectedFood!.name)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.green)
+                        .transition(.scale)
+                    
+                    Button(role: .none){
+                        showInfo = true
+                    } label: {
+                        Text("營養資訊")
+                    }
+                        .buttonStyle(.bordered)
+                    
+                }
+                
+                if showInfo {
+                    Text("熱量 \(Int(selectedFood!.calorie.rounded())) 大卡")
+                        .font(.title)
+                    
+                    HStack{
+                        Group{
+                            VStack{
+                                Text("蛋白質")
+                                Divider()
+                                Text("\(Int(selectedFood!.protein.rounded())) g")
+                            }
+                            VStack{
+                                Text("脂肪")
+                                Divider()
+                                Text("\(Int(selectedFood!.fat.rounded())) g")
+                            }
+                            VStack{
+                                Text("碳水")
+                                Divider()
+                                Text("\(Int(selectedFood!.carb.rounded())) g")
+                            }
+                        }
+                        .padding([.leading, .trailing], 5)
+                    }
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                }
                 
             }
             
+            // Color.clear
+            Spacer()
+            
             Button(role: .none){
                 selectedFood = food.shuffled().first
+                showInfo = false
             } label: {
-                Text( selectedFood == .none ? "告訴我" : "換一個" )
+                Text( selectedFood == nil ? "告訴我" : "換一個" )
                     .frame(width: 200, alignment: .center)
             }
                 .font(.title)
@@ -55,7 +111,7 @@ struct ContentView: View {
         }
         .padding()
         .animation(.easeInOut, value: selectedFood)
-        .frame(maxHeight: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.secondarySystemBackground))
     }
 }
