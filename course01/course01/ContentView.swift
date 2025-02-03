e550ad480f6c77d08b8bd35757a7beaf15b4cc03
Hunag Youci <huangyouci@icloud.com>
2025-02-04 00:23:10 +0800

    20250203-push01

--- course01/course01/ContentView.swift
+++ course01/course01/ContentView.swift
@@ -15,106 +15,122 @@ struct ContentView: View {
     @State private var showInfo: Bool = false
     
     var body: some View {
-        VStack (spacing: 30) {
-            
-            if selectedFood != .none {
+        ScrollView {
+            VStack (spacing: 30) {
                 
-                Text(selectedFood!.image)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.7)
-                    .lineLimit(1)
+                if selectedFood != .none {
                     
-            } else {
-                
-                Image("dinner")
-                    .resizable()
-                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
-            }
-                
-            Text("今天吃什麼？")
-                .font(.title)
-                .bold()
-            
-            if selectedFood != .none {
-                
-                HStack{
+                    Text(selectedFood!.image)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.7)
+                        .lineLimit(1)
                     
-                    Text(selectedFood!.name)
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundStyle(.green)
-                        .transition(.scale)
+                } else {
                     
-                    Button(role: .none){
-                        showInfo = true
-                    } label: {
-                        Text("營養資訊")
-                    }
-                        .buttonStyle(.bordered)
-                    
+                    Image("dinner")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                 }
                 
-                if showInfo {
-                    Text("熱量 \(Int(selectedFood!.calorie.rounded())) 大卡")
-                        .font(.title)
+                Text("今天吃什麼？")
+                    .font(.title)
+                    .bold()
+                
+                if selectedFood != .none {
                     
                     HStack{
-                        Group{
-                            VStack{
-                                Text("蛋白質")
-                                Divider()
-                                Text("\(Int(selectedFood!.protein.rounded())) g")
-                            }
-                            VStack{
-                                Text("脂肪")
-                                Divider()
-                                Text("\(Int(selectedFood!.fat.rounded())) g")
-                            }
-                            VStack{
-                                Text("碳水")
-                                Divider()
-                                Text("\(Int(selectedFood!.carb.rounded())) g")
-                            }
+                        
+                        Text(selectedFood!.name)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.green)
+                            .transition(.scale)
+                        
+                        Button(role: .none){
+                            showInfo = true
+                        } label: {
+                            Text("營養資訊")
                         }
-                        .padding([.leading, .trailing], 5)
+                        .buttonStyle(.bordered)
+                        
                     }
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(10)
+                    
+                    VStack{
+                        if showInfo {
+                            Text("熱量 \(Int(selectedFood!.calorie.rounded())) 大卡")
+                                .font(.title)
+                            
+                            Grid(verticalSpacing: 12) {
+                                GridRow {
+                                    Text("蛋白質")
+                                    Text("脂肪")
+                                    Text("碳水")
+                                }.frame(minWidth: 60)
+                                
+                                Divider()
+                                    .gridCellUnsizedAxes(.horizontal)
+                                    .padding(.horizontal, -10)
+                                
+                                GridRow{
+                                    Text("\(Int(selectedFood!.protein.rounded())) g")
+                                    Text("\(Int(selectedFood!.fat.rounded())) g")
+                                    Text("\(Int(selectedFood!.carb.rounded())) g")
+                                }
+                            }
+                            .padding()
+                            .background(Color(.white))
+                            .cornerRadius(10)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .clipped()
                 }
                 
-            }
-            
-            // Color.clear
-            Spacer()
-            
-            Button(role: .none){
-                selectedFood = food.shuffled().first
-                showInfo = false
-            } label: {
-                Text( selectedFood == nil ? "告訴我" : "換一個" )
-                    .frame(width: 200, alignment: .center)
-            }
+                Spacer()
+                
+                Button(role: .none){
+                    selectedFood = food.shuffled().first
+                } label: {
+                    Text( selectedFood == nil ? "告訴我" : "換一個" )
+                        .frame(width: 200, alignment: .center)
+                }
                 .font(.title)
                 .buttonStyle(.borderedProminent)
                 .controlSize(.large)
                 .padding(.bottom, -15)
                 .animation(.none, value: selectedFood)
-            
-            Button("取消") {
-                selectedFood = .none
-            }
+                
+                Button("取消") {
+                    selectedFood = .none
+                    showInfo = false
+                }
                 .font(.title)
                 .buttonStyle(.bordered)
-            
-            
+    
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height - 100)
         }
-        .padding()
-        .animation(.easeInOut, value: selectedFood)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.secondarySystemBackground))
+        .animation(.spring(dampingFraction: 0.55), value: showInfo)
+        .animation(.easeInOut, value: selectedFood)
     }
 }
+
+//extension ContentView {
+//    init(selectedFood: Food){
+//        _selectedFood = State(wrappedValue: selectedFood)
+//    }
+//}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView(selectedFood: .examples.first!)
+//        ContentView(selectedFood: .examples.first!).previewDevice(PreviewDevice(rawValue: "iPad Pro 13-inch (M4)"))
+//        ContentView(selectedFood: .examples.first!).previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
+//    }
+//}
 
 #Preview {
     ContentView()
