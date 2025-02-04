1b8ab55c44990b24ebd177817e68a58592a7001d
Hunag Youci <huangyouci@icloud.com>
2025-02-04 22:10:49 +0800

    20250204-push01

--- course01/course01/ContentView.swift
+++ course01/course01/ContentView.swift
@@ -9,82 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let food = Food.examples
-    
     @State private var selectedFood: Food?
     @State private var showInfo: Bool = false
+    
+    let food = Food.examples
+    
+    var foodImage: some View {
+        Group{
+            if selectedFood != .none {
+                Text(selectedFood!.image)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.7)
+                    .lineLimit(1)
+            } else {
+                Image("dinner")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+    
+    @ViewBuilder var selectedFoodView: some View {
+        if selectedFood != .none {
+            
+            HStack{
+                
+                Text(selectedFood!.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.green)
+                    .transition(.scale)
+                
+                Button(role: .none){
+                    showInfo = true
+                } label: {
+                    Text("營養資訊")
+                }
+                .buttonStyle(.bordered)
+                
+            }
+            
+            VStack{
+                if showInfo {
+                    Text("熱量 \(Int(selectedFood!.calorie.rounded())) 大卡")
+                        .font(.title)
+                    
+                    Grid(verticalSpacing: 12) {
+                        GridRow {
+                            Text("蛋白質")
+                            Text("脂肪")
+                            Text("碳水")
+                        }.frame(minWidth: 60)
+                        
+                        Divider()
+                            .gridCellUnsizedAxes(.horizontal)
+                            .padding(.horizontal, -10)
+                        
+                        GridRow{
+                            Text("\(Int(selectedFood!.protein.rounded())) g")
+                            Text("\(Int(selectedFood!.fat.rounded())) g")
+                            Text("\(Int(selectedFood!.carb.rounded())) g")
+                        }
+                    }
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .clipped()
+        }
+    }
     
     var body: some View {
         ScrollView {
             VStack (spacing: 30) {
                 
-                if selectedFood != .none {
-                    
-                    Text(selectedFood!.image)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.7)
-                        .lineLimit(1)
-                    
-                } else {
-                    
-                    Image("dinner")
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
-                }
+                foodImage
                 
                 Text("今天吃什麼？")
                     .font(.title)
                     .bold()
                 
-                if selectedFood != .none {
-                    
-                    HStack{
-                        
-                        Text(selectedFood!.name)
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundStyle(.green)
-                            .transition(.scale)
-                        
-                        Button(role: .none){
-                            showInfo = true
-                        } label: {
-                            Text("營養資訊")
-                        }
-                        .buttonStyle(.bordered)
-                        
-                    }
-                    
-                    VStack{
-                        if showInfo {
-                            Text("熱量 \(Int(selectedFood!.calorie.rounded())) 大卡")
-                                .font(.title)
-                            
-                            Grid(verticalSpacing: 12) {
-                                GridRow {
-                                    Text("蛋白質")
-                                    Text("脂肪")
-                                    Text("碳水")
-                                }.frame(minWidth: 60)
-                                
-                                Divider()
-                                    .gridCellUnsizedAxes(.horizontal)
-                                    .padding(.horizontal, -10)
-                                
-                                GridRow{
-                                    Text("\(Int(selectedFood!.protein.rounded())) g")
-                                    Text("\(Int(selectedFood!.fat.rounded())) g")
-                                    Text("\(Int(selectedFood!.carb.rounded())) g")
-                                }
-                            }
-                            .padding()
-                            .background(Color(.white))
-                            .cornerRadius(10)
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                    .clipped()
-                }
+                selectedFoodView
                 
                 Spacer()
                 
@@ -94,18 +101,21 @@ struct ContentView: View {
                     Text( selectedFood == nil ? "告訴我" : "換一個" )
                         .frame(width: 200, alignment: .center)
                 }
-                .font(.title)
+                .mainButtonStyle()
                 .buttonStyle(.borderedProminent)
-                .controlSize(.large)
                 .padding(.bottom, -15)
                 .animation(.none, value: selectedFood)
                 
-                Button("取消") {
+                Button(role: .none) {
                     selectedFood = .none
                     showInfo = false
+                } label: {
+                    Text("取消")
+                        .frame(width: 200, alignment: .center)
                 }
-                .font(.title)
+                .mainButtonStyle()
                 .buttonStyle(.bordered)
+                
     
                 
             }
@@ -115,6 +125,14 @@ struct ContentView: View {
         .background(Color(.secondarySystemBackground))
         .animation(.spring(dampingFraction: 0.55), value: showInfo)
         .animation(.easeInOut, value: selectedFood)
+    }
+}
+
+extension View {
+    func mainButtonStyle() -> some View {
+        font(.title)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
     }
 }
 
