089ee36fcb2ed9b1137be0eb657aefd5aeac2918
Hunag Youci <huangyouci@icloud.com>
2025-02-13 11:28:26 +0800

    建立 myProject03、忽略所有 .DS_Store 系統檔案

---
+++ myProject03/myProject03/AppEntry.swift
@@ -0,0 +1,17 @@
+//
+//  myProject03App.swift
+//  myProject03
+//
+//  Created by 黃宥琦 on 2025/2/13.
+//
+
+import SwiftUI
+
+@main
+struct myProject03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myProject03/myProject03/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myProject03
+//
+//  Created by 黃宥琦 on 2025/2/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
